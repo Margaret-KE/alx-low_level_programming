e22eaf72b4256181be462450b4d46d1fc8d702e2
queenmaggie <wanjirumargaret394@gmail.com>
2023-06-15 22:14:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - A C program that prints a line to the standard error
- * Return 0
+ * Return 1
  */
 int main(void)
 {
-	fput(and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
