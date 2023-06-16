5b0e6dbaa1283b46473fcff30a8afb0f52018d7e
queenmaggie <wanjirumargaret394@gmail.com>
2023-06-15 19:49:39 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints the size of various types based on comiler type
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu bytes(s)\n", size of(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(n)\n", sizeof(float));
+
+	return (0);
+}
